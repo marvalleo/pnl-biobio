8f69f8f8106c2854cc17e40cb32f9d0377f4487f
MML <martinezmarce@gmail.com>
2026-02-22 20:35:02 -0300

    feat: Implement network-first caching and skip waiting in the service worker and refine button CSS in `index.html`.

--- sw.js
+++ sw.js
@@ -8,17 +8,14 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+        fetch(event.request).catch(() => caches.match(event.request))
     );
 });
